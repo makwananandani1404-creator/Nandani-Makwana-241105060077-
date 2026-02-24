ddfcce6ddc9e1a262a93b9316fbb08e285287f4f
makwananandani1404-creator <makwananandani1404@gmail.com>
2026-02-24 20:33:35 +0530

    Add files via upload

---
+++ error  Multiplication Table Generator.py
@@ -0,0 +1,18 @@
+print("Multiplication Table Generator")
+num = int(input("Enter number: "))
+i = 1
+total = 0
+while i <= 10:
+      result = num * i
+      print(num, "x", i, "=", result)
+      if result % 2 = 0:
+            print("Even result")
+      else:
+            print("Odd result")
+      total = total + result
+      i = i + 1
+print("Sum of table:", total
+if total > 100
+      print("Large sum")
+else:
+      print("Small sum")
